ddd416579d0d2172b26302db3fb697abe7aad0ca
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2025-06-17 21:45:24 -0300

    remove polygon navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,14 @@ import { Box, Flex, Link, HStack } from "@chakra-ui/react";
 
 function NavBar() {
   return (
-    <Box bg="gray.800" px={4} py={8} fontFamily="Roboto, sans-serif" fontSize="1.3rem" paddingLeft="4%">
-      <Box bg="#23222a" w="150px" h="95px" position="absolute" right="0" top="0" zIndex="1" _hover={{ backgroundColor: "red.600" }} style={{
-        clipPath: "polygon(calc(0% + 40px) 0%, 100% 0%, calc(100% - 0px) 100%, 0% 100%)"
-      }}></Box>
+    <Box
+      bg="gray.800"
+      px={4}
+      py={8}
+      fontFamily="Roboto, sans-serif"
+      fontSize="1.3rem"
+      paddingLeft="4%"
+    >
       <Flex justifyContent="space-between" alignItems="center">
         <HStack gap={8}>
           <Link href="#" fontSize="xl" fontWeight="bold" color="white">
@@ -34,13 +38,19 @@ function NavBar() {
             Login
           </Link>
 
-          <Link href="#" color="white" mr={6} zIndex="1" _hover={{ color: "red.600" }}>
+          <Link
+            href="#"
+            color="white"
+            mr={6}
+            zIndex="1"
+            _hover={{ color: "red.600" }}
+          >
             Register
           </Link>
         </HStack>
       </Flex>
     </Box>
-  )
+  );
 }
 
 export default NavBar;
